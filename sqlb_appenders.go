7cc33c04021b5289aa7bd3fa052be4e597a2e6bc
Vladimir Medinskiy <v.medinskiy@corp.mail.ru>
2025-09-05 23:21:48 +0300

    added int64 and quated string appender

---
+++ sqlb_appenders.go
@@ -0,0 +1,23 @@
+package sqlb
+
+type Int64A int64
+
+func (a Int64A) String() string {
+	res, _ := String(int64(a))
+	return res
+}
+
+func (a Int64A) AppendTo(buf []byte) []byte {
+	out := append(buf, []byte(a.String())...)
+	return out
+}
+
+type StringQA string
+
+func (sq StringQA) String() string {
+	return "'" + Str(sq).String() + "'"
+}
+
+func (sq StringQA) AppendTo(buf []byte) []byte {
+	return Str(sq.String()).AppendTo(buf)
+}
